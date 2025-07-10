0511ff9866e6e993d38c27abb0aef459dd49b678
Fernando Herrero <herrerofernando.94@gmail.com>
2025-07-10 16:48:15 +0200

    feat(AddFruitList): create fruit list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { AddFruitForm } from "./components/AddFruitForm/AddFruitForm";
 import { NameList } from "./components/NameList/NameList";
 import { ProductList } from "./components/ProductList/ProductList";
 import { TaskList } from "./components/TaskList/TaskList";
@@ -9,6 +10,7 @@ function App() {
 			<NameList />
 			<ProductList />
 			<TaskList />
+			<AddFruitForm />
 		</>
 	);
 }
---
+++ src/components/AddFruitForm/AddFruitForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./AddFruitForm.css";
+
+export const AddFruitForm = () => {
+	const [fruits, setFruits] = useState([]);
+	const [addFruit, setAddFruit] = useState("");
+	const [error, setError] = useState("");
+
+	const onAddFruit = (event) => {
+		setError("");
+		setAddFruit(event.target.value);
+	};
+
+	const onSubmitForm = (event) => {
+		event.preventDefault();
+
+		if (!addFruit.trim()) return setError("Introduce a fruit");
+
+		if (addFruit) {
+			setFruits([...fruits, addFruit.trim()]);
+			setAddFruit("");
+			setError("");
+		}
+	};
+
+	return (
+		<div className="container-fruits-form">
+			<form className="-fruits-form" onSubmit={onSubmitForm}>
+				<label htmlFor="fruit"></label>
+				<input type="text" id="fruit" autoComplete="text" name="fruit" value={addFruit} onChange={onAddFruit} />
+				<button type="submit">Add</button>
+			</form>
+
+			{fruits.length > 0 && (
+				<div className="fruits-list">
+					<h1>Fruits List</h1>
+					<ul>
+						{fruits.map((fruit, index) => (
+							<li key={`${fruit}-${index}`}>{fruit}</li>
+						))}
+					</ul>
+				</div>
+			)}
+
+			{error && <span className="error">{error}</span>}
+		</div>
+	);
+};
